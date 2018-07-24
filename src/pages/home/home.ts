0a83336aa51f8dff6b832d3cc2cb65fa5cc3f057
devlitus <developercarles@gmail.com>
2018-07-24 08:41:57 +0200

    nuevos cambios

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  datos;
+  constructor(public navCtrl: NavController, private fb: Facebook, public toastCtrl: ToastController) {}
 
-  constructor(public navCtrl: NavController) {
-
+  loginFacebook(){
+    this.fb.login(['public_profile', 'email'])
+    .then((res: FacebookLoginResponse) => {
+      if(res.status == 'connected'){
+        this.createToast(res.status);
+        this.account();
+        console.log(JSON.stringify(res));
+      }
+    })
+    .catch(e => {this.createToast('No connected');})
+  }
+  account(){
+    this.fb.api('/me/accounts', ['manage_pages', 'publish_pages'])
+    .then(res => {
+      this.datos = res.data;
+      // console.log(JSON.stringify(res));
+    })
+    .catch(e => {console.error("fallo publish ", e);})
+  }
+  createToast(message){
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    })
+    toast.present();
+  }
+  logout(){
+    this.fb.logout()
+    .then(result =>{this.createToast("Adios")})
   }
 
 }
